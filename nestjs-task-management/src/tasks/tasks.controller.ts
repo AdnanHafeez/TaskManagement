946c4a225bbb8cb08618c2e48ab4ad175ecdf32b
AdnanHafeez <ahafeez@deakin.edu.au>
2020-07-29 16:56:45 +1000

    fixed uuid

--- nestjs-task-management/src/tasks/tasks.controller.ts
+++ nestjs-task-management/src/tasks/tasks.controller.ts
@@ -1,13 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body, Logger } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
 
 @Controller('tasks')
 export class TasksController {
-    constructor(private tasksService: TasksService){}
+    constructor(private tasksService: TasksService,
+        private logger: Logger){
+            logger.setContext('TasksController');
+        }
 
     @Get()
     getAllTasks(): Task[] {
         return this.tasksService.getAllTasks();
+    }
+
+    @Post()
+    createTask(
+        @Body('title') title: string,
+        @Body('description') description: string,
+    ) : Task {
+        this.logger.verbose(`Title:${title}`);
+        this.logger.verbose(`Description:${description}`);
+        return this.tasksService.createTask(title,description);
     }
 }
--- nestjs-task-management/src/tasks/tasks.service.ts
+++ nestjs-task-management/src/tasks/tasks.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
-import * as uuid from 'uuid/dist/v1';
+import { v4 as uuidv4} from 'uuid';
 
 @Injectable()
 export class TasksService {
@@ -12,7 +12,7 @@ export class TasksService {
 
     createTask(title: string, description: string) : Task{
         const task: Task = {
-            id: uuid(),
+            id: uuidv4(),
             title,
             description,
             status: TaskStatus.OPEN,
