eb10869c57bee2df6e927c5414ecbee2aa9df34e
AdnanHafeez <ahafeez@deakin.edu.au>
2020-08-01 16:12:55 +1000

    user repository

--- nestjs-task-management/src/auth/auth.module.ts
+++ nestjs-task-management/src/auth/auth.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { UserRespository } from './user.respository';
 
 @Module({
+  imports:[
+    TypeOrmModule.forFeature([UserRespository])
+  ],
   controllers: [AuthController],
   providers: [AuthService]
 })
--- nestjs-task-management/src/auth/auth.service.ts
+++ nestjs-task-management/src/auth/auth.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { UserRespository } from './user.respository';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    
+    constructor(
+        @InjectRepository(UserRespository)
+        private userRepository: UserRespository){}
+}
